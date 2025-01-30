6fb2656e74608992d60e95e9a9352c098db31554
iamfarazkhan7 <iamfarazkhan7@gmai.com>
2025-01-30 14:58:47 +0500

    Section 2 app creation and bassic layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import ProductList from "@/components/shared/header/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Homepage = () => {
-  return ( <>Prostore</> );
-}
- 
+  return (
+    <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
+  );
+};
+
 export default Homepage;
