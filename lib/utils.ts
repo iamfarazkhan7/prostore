6323144cbfb19dcf7a0dbcfeb2af532c235583eb
iamfarazkhan7 <iamfarazkhan7@gmai.com>
2025-02-17 15:51:38 +0500

    section 4 ends

--- lib/utils.ts
+++ lib/utils.ts
@@ -15,3 +15,29 @@ export function formatNumberWithDecimal(num: number): string {
   const [int, decimal] = num.toString().split(".");
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
+
+//format errors
+//// eslint-disable-next-line @typesscript-eslint/no-explicit-any
+export function formatError(error: any) {
+  if (error.name === "ZodError") {
+    // zod errors
+
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
+
+    return fieldErrors.join(". ");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    //prisma errors
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists.`;
+  } else {
+    //other errors
+    return error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
+  }
+}
