2a148929f03e7cb4a9d27c595ed581aaf237673b
iamfarazkhan7 <iamfarazkhan7@gmai.com>
2025-02-27 19:26:15 +0500

    section 6 part 4

---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,18 @@
+import { title } from "process";
+import CartTable from "./cart-table";
+import { getMyCart } from "@/lib/actions/cart.actions";
+
+export const metadata = {
+  title: "Shopping Cart",
+};
+
+const CartPage = async () => {
+  const cart = await getMyCart();
+  return (
+    <>
+      <CartTable cart={cart} />
+    </>
+  );
+};
+
+export default CartPage;
